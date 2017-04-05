f17f648affb17ffdb950f43d5df7608e48f6bd4d
Wes Modes <wmodes@gmail.com>
2017-04-05 18:50:33 +0200

    typos

--- fomoserver/config.py
+++ fomoserver/config.py
@@ -1,13 +1,13 @@
+from secrets import *
+
 warehouse_format = "pop"
 pop_host = "pop.gmail.com"
 pop_port = 995
 pop_account = "fomophobiadata@gmail.com"
-pop_password = "Ih@t3TwitterRilly"
 db_host = "localhost"
 db_port = 3306
 db_name = "fomophobia"
 db_user = "fomophobia"
-db_password = "Ih@t3Twitter"
 types = ["email",
     "gmail",
     "twitter",
